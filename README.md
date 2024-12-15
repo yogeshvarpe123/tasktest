98180ccd3297347c8e6c88931969e7391d4ad34e
varpe <yogeshvarpe96@gmail.com>
2024-12-15 20:58:04 +0530

    util changes

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-this is the Python CLI application that will help you parse logs of various kinds
+Interview app - log parsing utility v1.01:
+
+This is a simple Python utility for parsing and filtering log files. It allows you to extract specific lines based on timestamps, IPv4/IPv6 addresses, or line counts.
+
+Usage
+./util.py [OPTION]... [FILE]
+
+Options:
+-f, --first NUM  : Print the first NUM lines.
+-l, --last NUM   : Print the last NUM lines.
+-t, --timestamps : Print lines containing timestamps (HH:MM:SS).
+-i, --ipv4       : Print lines with IPv4 addresses (highlighted).
+-I, --ipv6       : Print lines with IPv6 addresses (highlighted).
+
+Examples:
+
+1)Print the first 10 lines:
+python ./util.py -f 10 test_0.log
+
+2)Print the last 10 lines:
+python ./util.py -l 10 test_0.log
+
+3)print lines with IPv4 addresses:
+python ./util.py --ipv4 test_0.log
+
+4)print lines with IPv6 addresses:
+python ./util.py --ipv6 test_0.log
+
+5)print lines with timestamps addresses:
+python ./util.py -t test_0.log
+
+
+
+Testing
+
+Run the test suite using unittest:
+python -m unittest test_util.py
+
+Files
+util.py : The main log parsing script.
+test_util.py : Unit tests for the utility.
+test_0.log : Example log file for testing.
--- util.py
+++ util.py
@@ -4,16 +4,35 @@ import argparse
 import re
 import sys
 
+class CustomHelpFormatter(argparse.HelpFormatter):
+    def _get_help_string(self, action):
+        """Customize help string to exclude default values."""
+        return action.help  # Return the help text without the default info
+
+    def _format_action_invocation(self, action):
+        """Customize action invocation to show options with '=NUM'."""
+        if action.option_strings:
+            options = ', '.join(action.option_strings)
+            if action.metavar:
+                return f"{options}={action.metavar}"  # Add '=NUM' explicitly
+            return options
+        return super()._format_action_invocation(action)
+        
+
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Log Parsing Utility v1.01"
+        prog='./util.py',
+        usage='./util.py [OPTION]... [FILE]',
+        add_help=False,
+        formatter_class=CustomHelpFormatter
     )
-    parser.add_argument("file", nargs="?", type=argparse.FileType('r'), default=sys.stdin, help="Input file or stdin")
-    parser.add_argument("-f", "--first", type=int, help="Print first NUM lines")
-    parser.add_argument("-l", "--last", type=int, help="Print last NUM lines")
-    parser.add_argument("-t", "--timestamps", action="store_true", help="Print lines containing timestamps (HH:MM:SS)")
+    parser.add_argument("-h", "--help", action="help", help="Print help")
+    parser.add_argument("file", nargs="?", type=argparse.FileType('r'), default=sys.stdin, help=argparse.SUPPRESS)
+    parser.add_argument("-f", "--first", type=int, metavar="NUM", help="Print first NUM lines")
+    parser.add_argument("-l", "--last", type=int, metavar="NUM", help="Print last NUM lines")
+    parser.add_argument("-t", "--timestamps", action="store_true", help="Print lines that contain a timestamp in HH:MM:SS format")
     parser.add_argument("-i", "--ipv4", action="store_true", help="Print lines containing IPv4 addresses, with matches highlighted")
-    parser.add_argument("-I", "--ipv6", action="store_true", help="Print lines containing IPv6 addresses, with matches highlighted")
+    parser.add_argument("-I", "--ipv6", action="store_true", help="Print lines that contain an IPv6 address (standard notation), matching IPs are highlighted")
     return parser.parse_args()
 
 def highlight(text, regex):
@@ -28,10 +47,10 @@ def process_lines(lines, args):
     if args.timestamps:
         lines = [line for line in lines if re.search(r"\b\d{2}:\d{2}:\d{2}\b", line)]
     if args.ipv4:
-        ipv4_regex = r"\b(?:\d{1,3}\.){3}\d{1,3}\b"
+        ipv4_regex = r"\b((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\b"
         lines = [highlight(line, ipv4_regex) for line in lines if re.search(ipv4_regex, line)]
     if args.ipv6:
-        ipv6_regex = r"\b([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}\b"
+        ipv6_regex = r"\b(([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|::)\b"
         lines = [highlight(line, ipv6_regex) for line in lines if re.search(ipv6_regex, line)]
     
     return lines
